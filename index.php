0526a2df846d56923de5ed5142476974b1596d72
LAPTOP-NFEUPJL2\marko <99045897@mydavinci.nl>
2018-01-24 09:20:11 +0100

    Goede PHP opdracht

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+  date_default_timezone_set('Europe/Amsterdam');
+
+  $time = date("h:i:s");
+  $text = "";
+  $img = "";
+
+  $t = date('H');
+
+    if ($t < "6") {
+    $text = "Goede nacht";
+      $img = "night.png";
+  } elseif ($t < "12") {
+    $text = "Goede morgen!";
+      $img = "morning.png";
+  } elseif ($t < "18") {
+    $text = "Goede middag!";
+      $img = "afternoon.png";
+  } elseif ($t < "24") {
+    $text = "Goede avond!";
+      $img = "evening.png";
+  }
+//  header("Refresh:1");
+ ?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <title>De tijd</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="style.css">
+
+  </head>
+
+  <body background="<?php echo $img ?>">
+    <main id="main">
+
+      <?php
+      echo $time;
+      echo $text;
+      ?>
+
+      <button id="button" onclick="restart()">Reload</button>
+
+    </main>
+
+    <script src="script.js" type="text/javascript"></script>
+
+  </body>
+
+</html>
